d2843be82400d995c1903286ddf2bb26c1a0dc27
Lam Islam <lamislam4444@gmail.com>
2024-06-18 21:35:15 +0600

    Done✅

---
+++ laravel-file-upload/app/Http/Controllers/ProfileController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+
+class ProfileController extends Controller
+{
+
+    public function index($id)
+    {
+            $name = "Donal Trump";
+            $age ="75";
+            $data = ["id"=>$id,"name" => $name, "age" => $age];
+
+
+            $cookieName = 'access_token';
+            $value = '123-XYZ';
+            $minutes = 1;
+            $path = '/';
+            $domain = $_SERVER['SERVER_NAME'];
+            $secure = false;
+            $httponly = true;
+
+
+
+        return response()
+            ->json($data , 200)
+            ->cookie($cookieName , $value, $minutes, $path, $domain, $secure, $httponly);
+
+
+    }
+
+}
